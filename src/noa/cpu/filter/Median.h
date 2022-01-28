27275ba0d13fbe3ccb507ca215051314d1934da0
ffyr2w <tomfros@hotmail.fr>
2022-01-28 02:28:36 +0100

    `noa::cpu::filter` - Switch to rightmost order and strides
    Tags: [feature]
    Compile: no
    Tests: no

--- src/noa/cpu/filter/Median.h
+++ src/noa/cpu/filter/Median.h
@@ -11,61 +11,58 @@
 
 namespace noa::cpu::filter {
     /// Computes the median filter using a 1D window.
-    /// \tparam T               int, uint, float or double.
-    /// \param[in] inputs       On the \b host. Array to filter. One per batch.
-    /// \param input_pitch      Pitch, in elements, of \p inputs.
-    /// \param[out] outputs     On the \b host. Filtered array. One per batch.
-    /// \param input_pitch      Pitch, in elements, of \p outputs.
-    /// \param shape            Logical {fast, medium, slow} shape of \p inputs and \p outputs, in \p T elements.
-    /// \param batches          Number of batches to filter.
+    /// \tparam T               (u)int, (u)long, (u)long long, half_t, float, double.
+    /// \param[in] input        On the \b host. Array to filter.
+    /// \param input_stride     Rightmost strides, in elements, of \p input.
+    /// \param[out] output      On the \b host. Filtered array.
+    /// \param input_stride     Rightmost strides, in elements, of \p output.
+    /// \param shape            Rightmost shape of \p input and \p output.
     /// \param border_mode      Border mode used for the "implicit padding". Either BORDER_ZERO or BORDER_REFLECT.
     /// \param window_size      Number of elements to consider for the computation of the median.
-    ///                         This corresponds to the first dimension of \p shape.
+    ///                         This corresponds to the innermost dimension.
     ///                         Only odd numbers are supported. If 1, a copy is performed.
     /// \param[in,out] stream   Stream on which to enqueue this function.
     /// \note Depending on the stream, this function may be asynchronous and may return before completion.
-    /// \note With \c BORDER_REFLECT, the first dimension should be larger or equal than `window_size/2 + 1`.
-    /// \note \p inputs and \p outputs should not overlap.
+    /// \note With \c BORDER_REFLECT, the innermost dimension should be >= than ``window_size/2 + 1``.
+    /// \note \p input and \p output should not overlap.
     template<typename T>
-    NOA_HOST void median1(const T* inputs, size3_t input_pitch, T* outputs, size3_t output_pitch,
-                          size3_t shape, size_t batches, BorderMode border_mode, size_t window_size, Stream& stream);
+    NOA_HOST void median1(const T* input, size4_t input_stride, T* output, size4_t output_stride,
+                          size4_t shape, BorderMode border_mode, size_t window_size, Stream& stream);
 
     /// Computes the median filter using a 2D square window.
-    /// \tparam T               int, uint, float or double.
-    /// \param[in] inputs       On the \b host. Array to filter. One per batch.
-    /// \param input_pitch      Pitch, in elements, of \p inputs.
-    /// \param[out] outputs     On the \b host. Filtered array. One per batch.
-    /// \param input_pitch      Pitch, in elements, of \p outputs.
-    /// \param shape            Logical {fast, medium, slow} shape of \p inputs and \p outputs, in \p T elements.
-    /// \param batches          Number of batches to filter.
+    /// \tparam T               (u)int, (u)long, (u)long long, half_t, float, double.
+    /// \param[in] input        On the \b host. Array to filter.
+    /// \param input_stride     Rightmost strides, in elements, of \p input.
+    /// \param[out] output      On the \b host. Filtered array.
+    /// \param input_stride     Rightmost strides, in elements, of \p output.
+    /// \param shape            Rightmost shape of \p input and \p output.
     /// \param border_mode      Border mode used for the "implicit padding". Either BORDER_ZERO, or BORDER_REFLECT.
     /// \param window_size      Number of elements to consider for the computation of the median, for each dimension.
-    ///                         This corresponds to the first and second dimension of \p shape.
+    ///                         This corresponds to the second and innermost dimension.
     ///                         Only odd numbers are supported. If 1, no filter is applied.
     /// \param[in,out] stream   Stream on which to enqueue this function.
     /// \note Depending on the stream, this function may be asynchronous and may return before completion.
-    /// \note With \c BORDER_REFLECT, the first two dimensions should be larger or equal than `window_size/2 + 1`.
-    /// \note \p inputs and \p outputs should not overlap.
+    /// \note With \c BORDER_REFLECT, the second and innermost dimensions should be >= than ``window_size/2 + 1``.
+    /// \note \p input and \p output should not overlap.
     template<typename T>
-    NOA_HOST void median2(const T* inputs, size3_t input_pitch, T* outputs, size3_t output_pitch,
-                          size3_t shape, size_t batches, BorderMode border_mode, size_t window_size, Stream& stream);
+    NOA_HOST void median2(const T* input, size4_t input_stride, T* output, size4_t output_stride,
+                          size4_t shape, BorderMode border_mode, size_t window_size, Stream& stream);
 
     /// Computes the median filter using a 3D cubic window.
-    /// \tparam T               int, uint, float or double.
-    /// \param[in] inputs       On the \b host. Array to filter. One per batch.
-    /// \param input_pitch      Pitch, in elements, of \p inputs.
-    /// \param[out] outputs     On the \b host. Filtered array. One per batch.
-    /// \param input_pitch      Pitch, in elements, of \p outputs.
-    /// \param shape            Logical {fast, medium, slow} shape of \p inputs and \p outputs, in \p T elements.
-    /// \param batches          Number of batches to filter.
+    /// \tparam T               (u)int, (u)long, (u)long long, half_t, float, double.
+    /// \param[in] input        On the \b host. Array to filter.
+    /// \param input_stride     Rightmost strides, in elements, of \p input.
+    /// \param[out] output      On the \b host. Filtered array.
+    /// \param input_stride     Rightmost strides, in elements, of \p output.
+    /// \param shape            Rightmost shape of \p input and \p output.
     /// \param border_mode      Border mode used for the "implicit padding". Either BORDER_ZERO, or BORDER_REFLECT.
     /// \param window_size      Number of elements to consider for the computation of the median, for each dimension.
     ///                         Only odd numbers are supported. If 1, no filter is applied.
     /// \param[in,out] stream   Stream on which to enqueue this function.
     /// \note Depending on the stream, this function may be asynchronous and may return before completion.
-    /// \note With \c BORDER_REFLECT, each dimension should be larger or equal than `window_size/2 + 1`.
-    /// \note \p inputs and \p outputs should not overlap.
+    /// \note With \c BORDER_REFLECT, each dimension should be >= than ``window_size/2 + 1``.
+    /// \note \p input and \p output should not overlap.
     template<typename T>
-    NOA_HOST void median3(const T* inputs, size3_t input_pitch, T* outputs, size3_t output_pitch,
-                          size3_t shape, size_t batches, BorderMode border_mode, size_t window_size, Stream& stream);
+    NOA_HOST void median3(const T* input, size4_t input_stride, T* output, size4_t output_stride,
+                          size4_t shape, BorderMode border_mode, size_t window_size, Stream& stream);
 }
--- src/noa/cpu/filter/ShapeCylinder.cpp
+++ src/noa/cpu/filter/ShapeCylinder.cpp
@@ -8,51 +8,51 @@ namespace {
     using namespace noa;
 
     template<bool INVERT>
-    float getSoftMask_(float distance_xy_sqd, float radius_xy_sqd, float radius_xy, float radius_xy_sqd_with_taper,
-                       float distance_z, float radius_z, float radius_z_with_taper, float taper_size) {
+    float getSoftMask_(float distance_kl_sqd, float radius_sqd, float radius, float radius_sqd_with_taper,
+                       float distance_j, float length, float length_with_taper, float taper_size) {
         constexpr float PI = math::Constants<float>::PI;
         float mask;
         if constexpr (INVERT) {
-            if (distance_z > radius_z_with_taper || distance_xy_sqd > radius_xy_sqd_with_taper) {
+            if (distance_j > length_with_taper || distance_kl_sqd > radius_sqd_with_taper) {
                 mask = 1.f;
             } else {
-                if (distance_xy_sqd <= radius_xy_sqd) {
+                if (distance_kl_sqd <= radius_sqd) {
                     mask = 1.f;
                 } else {
-                    float distance_xy = math::sqrt(distance_xy_sqd);
-                    mask = (1.f + math::cos(PI * (distance_xy - radius_xy) / taper_size)) * 0.5f;
+                    float distance_kl = math::sqrt(distance_kl_sqd);
+                    mask = (1.f + math::cos(PI * (distance_kl - radius) / taper_size)) * 0.5f;
                 }
-                if (distance_z > radius_z)
-                    mask *= (1.f + math::cos(PI * (distance_z - radius_z) / taper_size)) * 0.5f;
+                if (distance_j > length)
+                    mask *= (1.f + math::cos(PI * (distance_j - length) / taper_size)) * 0.5f;
                 mask = 1 - mask;
             }
         } else {
-            if (distance_z > radius_z_with_taper || distance_xy_sqd > radius_xy_sqd_with_taper) {
+            if (distance_j > length_with_taper || distance_kl_sqd > radius_sqd_with_taper) {
                 mask = 0.f;
             } else {
-                if (distance_xy_sqd <= radius_xy_sqd) {
+                if (distance_kl_sqd <= radius_sqd) {
                     mask = 1.f;
                 } else {
-                    float distance_xy = math::sqrt(distance_xy_sqd);
-                    mask = (1.f + math::cos(PI * (distance_xy - radius_xy) / taper_size)) * 0.5f;
+                    float distance_kl = math::sqrt(distance_kl_sqd);
+                    mask = (1.f + math::cos(PI * (distance_kl - radius) / taper_size)) * 0.5f;
                 }
-                if (distance_z > radius_z)
-                    mask *= (1.f + math::cos(PI * (distance_z - radius_z) / taper_size)) * 0.5f;
+                if (distance_j > length)
+                    mask *= (1.f + math::cos(PI * (distance_j - length) / taper_size)) * 0.5f;
             }
         }
         return mask;
     }
 
     template<bool INVERT>
-    float getHardMask_(float distance_xy_sqd, float radius_xy_sqd, float distance_z, float radius_z) {
+    float getHardMask_(float distance_kl_sqd, float radius_sqd, float distance_j, float length) {
         float mask;
         if constexpr (INVERT) {
-            if (distance_z > radius_z || distance_xy_sqd > radius_xy_sqd)
+            if (distance_j > length || distance_kl_sqd > radius_sqd)
                 mask = 1.f;
             else
                 mask = 0.f;
         } else {
-            if (distance_z > radius_z || distance_xy_sqd > radius_xy_sqd)
+            if (distance_j > length || distance_kl_sqd > radius_sqd)
                 mask = 0.f;
             else
                 mask = 1.f;
@@ -61,44 +61,39 @@ namespace {
     }
 
     template<bool TAPER, bool INVERT, typename T>
-    void cylinderOMP_(const T* inputs, size3_t input_pitch, T* outputs, size3_t output_pitch, size3_t shape,
-                      size_t batches, float3_t center, float radius_xy, float radius_z, float taper_size,
-                      size_t threads) {
+    void cylinderOMP_(const T* input, size4_t input_stride, T* output, size4_t output_stride, size4_t shape,
+                      float3_t center, float length, float radius, float taper_size, size_t threads) {
         using real_t = traits::value_type_t<T>;
-        const float radius_xy_sqd = radius_xy * radius_xy;
-        [[maybe_unused]] const float radius_xy_taper_sqd = math::pow(radius_xy + taper_size, 2.f);
-        [[maybe_unused]] const float radius_z_taper = radius_z + taper_size;
+        const float radius_sqd = radius * radius;
+        [[maybe_unused]] const float radius_taper_sqd = math::pow(radius + taper_size, 2.f);
+        [[maybe_unused]] const float length_plus_taper = length + taper_size;
 
         #pragma omp parallel for collapse(4) default(none) num_threads(threads) \
-        shared(inputs, input_pitch, outputs, output_pitch, shape, batches, center, radius_xy, radius_z, taper_size, \
-               radius_xy_sqd, radius_xy_taper_sqd, radius_z_taper)
+        shared(input, input_stride, output, output_stride, shape, center, length, radius, taper_size, \
+               length_plus_taper, radius_sqd, radius_taper_sqd)
 
-        for (size_t batch = 0; batch < batches; ++batch) {
-            for (size_t z = 0; z < shape.z; ++z) {
-                for (size_t y = 0; y < shape.y; ++y) {
-                    for (size_t x = 0; x < shape.x; ++x) {
+        for (size_t i = 0; i < shape[0]; ++i) {
+            for (size_t j = 0; j < shape[1]; ++j) {
+                for (size_t k = 0; k < shape[2]; ++k) {
+                    for (size_t l = 0; l < shape[3]; ++l) {
 
-                        const T* input = inputs + batch * elements(input_pitch);
-                        T* output = outputs + batch * elements(output_pitch);
-                        output += index(x, y, z, output_pitch);
-
-                        float2_t dst(x, y);
-                        dst -= {center.x, center.y};
+                        float2_t dst{k, l};
+                        dst -= {center[1], center[2]};
                         dst *= dst;
-                        const float dst_sd_xy = math::sum(dst);
-                        const float dst_z = math::abs(static_cast<float>(z) - center.z);
+                        const float dst_j = math::abs(static_cast<float>(j) - center[0]);
+                        const float dst_kl_sqd = math::sum(dst);
 
                         float mask;
                         if constexpr (TAPER)
-                            mask = getSoftMask_<INVERT>(dst_sd_xy, radius_xy_sqd, radius_xy, radius_xy_taper_sqd,
-                                                        dst_z, radius_z, radius_z_taper, taper_size);
+                            mask = getSoftMask_<INVERT>(dst_kl_sqd, radius_sqd, radius, radius_taper_sqd,
+                                                        dst_j, length, length_plus_taper, taper_size);
                         else
-                            mask = getHardMask_<INVERT>(dst_sd_xy, radius_xy_sqd, dst_z, radius_z);
+                            mask = getHardMask_<INVERT>(dst_kl_sqd, radius_sqd, dst_j, length);
 
-                        if (inputs)
-                            *output = input[index(x, y, z, input_pitch)] * static_cast<real_t>(mask);
-                        else
-                            *output = static_cast<real_t>(mask);
+                        output[at(i, j, k, l, output_stride)] =
+                                input ?
+                                input[at(i, j, k, l, input_stride)] * static_cast<real_t>(mask) :
+                                static_cast<real_t>(mask);
                     }
                 }
             }
@@ -106,38 +101,34 @@ namespace {
     }
 
     template<bool TAPER, bool INVERT, typename T>
-    void cylinder_(const T* inputs, size3_t input_pitch, T* outputs, size3_t output_pitch, size3_t shape,
-                   size_t batches, float3_t center, float radius_xy, float radius_z, float taper_size) {
+    void cylinder_(const T* input, size4_t input_stride, T* output, size4_t output_stride, size4_t shape,
+                   float3_t center, float radius, float length, float taper_size) {
         using real_t = traits::value_type_t<T>;
-        const float radius_xy_sqd = radius_xy * radius_xy;
-        [[maybe_unused]] const float radius_xy_taper_sqd = math::pow(radius_xy + taper_size, 2.f);
-        [[maybe_unused]] const float radius_z_taper = radius_z + taper_size;
+        const float radius_sqd = radius * radius;
+        [[maybe_unused]] const float radius_taper_sqd = math::pow(radius + taper_size, 2.f);
+        [[maybe_unused]] const float length_taper = length + taper_size;
 
-        for (size_t batch = 0; batch < batches; ++batch) {
-            const T* input = inputs + batch * elements(input_pitch);
-            T* output = outputs + batch * elements(output_pitch);
+        for (size_t i = 0; i < shape[0]; ++i) {
+            for (size_t j = 0; j < shape[1]; ++j) {
+                for (size_t k = 0; k < shape[2]; ++k) {
+                    for (size_t l = 0; l < shape[3]; ++l) {
 
-            for (size_t z = 0; z < shape.z; ++z) {
-                const float distance_z = math::abs(static_cast<float>(z) - center.z);
-                for (size_t y = 0; y < shape.y; ++y) {
-                    const float distance_y_sqd = math::pow(static_cast<float>(y) - center.y, 2.f);
-                    const size_t iffset = index(y, z, input_pitch);
-                    const size_t offset = index(y, z, output_pitch);
-                    for (size_t x = 0; x < shape.x; ++x) {
-                        float distance_xy_sqd = math::pow(static_cast<float>(x) - center.x, 2.f);
-                        distance_xy_sqd += distance_y_sqd;
+                        const float distance_z = math::abs(static_cast<float>(j) - center[0]);
+                        const float distance_y_sqd = math::pow(static_cast<float>(k) - center[1], 2.f);
+                        const float distance_x_sqd = math::pow(static_cast<float>(l) - center[2], 2.f);
+                        const float distance_xy_sqd = distance_y_sqd + distance_x_sqd;
 
                         float mask;
                         if constexpr (TAPER)
-                            mask = getSoftMask_<INVERT>(distance_xy_sqd, radius_xy_sqd, radius_xy, radius_xy_taper_sqd,
-                                                        distance_z, radius_z, radius_z_taper, taper_size);
+                            mask = getSoftMask_<INVERT>(distance_xy_sqd, radius_sqd, radius, radius_taper_sqd,
+                                                        distance_z, length, length_taper, taper_size);
                         else
-                            mask = getHardMask_<INVERT>(distance_xy_sqd, radius_xy_sqd, distance_z, radius_z);
+                            mask = getHardMask_<INVERT>(distance_xy_sqd, radius_sqd, distance_z, length);
 
-                        if (inputs)
-                            output[offset + x] = input[iffset + x] * static_cast<real_t>(mask);
-                        else
-                            output[offset + x] = static_cast<real_t>(mask);
+                        output[at(i, j, k, l, output_stride)] =
+                                input ?
+                                input[at(i, j, k, l, input_stride)] * static_cast<real_t>(mask) :
+                                static_cast<real_t>(mask);
                     }
                 }
             }
@@ -147,19 +138,18 @@ namespace {
 
 namespace noa::cpu::filter {
     template<bool INVERT, typename T>
-    void cylinder(const T* inputs, size3_t input_pitch, T* outputs, size3_t output_pitch, size3_t shape,
-                  size_t batches, float3_t center, float radius_xy, float radius_z, float taper_size,
-                  Stream& stream) {
+    void cylinder(const T* input, size4_t input_stride, T* output, size4_t output_stride, size4_t shape,
+                  float3_t center, float radius, float length, float taper_size, Stream& stream) {
         NOA_PROFILE_FUNCTION();
 
         // Instead of computing the mask for each batch, compute once and then copy to the other batches.
-        if (batches > 1 && !inputs) {
-            cylinder<INVERT>(inputs, input_pitch, outputs, output_pitch, shape, 1,
-                             center, radius_xy, radius_z, taper_size, stream);
-            for (size_t batch = 1; batch < batches; ++batch)
-                memory::copy(outputs, output_pitch,
-                             outputs + batch * elements(output_pitch), output_pitch,
-                             shape, 1, stream);
+        if (shape[0] > 1 && !input) {
+            const size4_t shape_3d{1, shape[1], shape[2], shape[3]};
+            cylinder<INVERT>(input, input_stride, output, output_stride, shape_3d,
+                             center, radius, length, taper_size, stream);
+            memory::copy(output, {0 /* reuse same batch */, output_stride[1], output_stride[2], output_stride[3]},
+                         output + output_stride[0] /* start copy into second batch */, output_stride,
+                         {shape[0] - 1 /* the first batch is the source */, shape[1], shape[2], shape[3]}, stream);
             return;
         }
 
@@ -167,17 +157,17 @@ namespace noa::cpu::filter {
         const bool taper = taper_size > 1e-5f;
         if (threads > 1)
             stream.enqueue(taper ? cylinderOMP_<true, INVERT, T> : cylinderOMP_<false, INVERT, T>,
-                           inputs, input_pitch, outputs, output_pitch, shape, batches,
-                           center, radius_xy, radius_z, taper_size, threads);
+                           input, input_stride, output, output_stride, shape,
+                           center, radius, length, taper_size, threads);
         else
             stream.enqueue(taper ? cylinder_<true, INVERT, T> : cylinder_<false, INVERT, T>,
-                           inputs, input_pitch, outputs, output_pitch, shape, batches,
-                           center, radius_xy, radius_z, taper_size);
+                           input, input_stride, output, output_stride, shape,
+                           center, radius, length, taper_size);
     }
 
-    #define NOA_INSTANTIATE_CYLINDER_(T)                                                                                        \
-    template void cylinder<true, T>(const T*, size3_t, T*, size3_t, size3_t, size_t, float3_t, float, float, float, Stream&);   \
-    template void cylinder<false, T>(const T*, size3_t, T*, size3_t, size3_t, size_t, float3_t, float, float, float, Stream&)
+    #define NOA_INSTANTIATE_CYLINDER_(T)                                                                                \
+    template void cylinder<true, T>(const T*, size4_t, T*, size4_t, size4_t, float3_t, float, float, float, Stream&);   \
+    template void cylinder<false, T>(const T*, size4_t, T*, size4_t, size4_t, float3_t, float, float, float, Stream&)
 
     NOA_INSTANTIATE_CYLINDER_(half_t);
     NOA_INSTANTIATE_CYLINDER_(float);
