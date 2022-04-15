db81bc8e7a52f332c2c933722c6fde04b5b0629a
ffyr2w <tomfros@hotmail.fr>
2022-04-15 11:22:15 +0200

    Add `NOA_ENABLE_UNIFIED` definitions to `noa`
    I forgot to include this in previous commits.
    
    Tags: []
    Compile: yes
    Tests: yes

--- cmake/targets/noa.cmake
+++ cmake/targets/noa.cmake
@@ -163,6 +163,7 @@ target_compile_definitions(noa_static
         "$<$<BOOL:${NOA_ENABLE_PROFILER}>:NOA_PROFILE>"
         "$<$<BOOL:${NOA_ENABLE_CPU}>:NOA_ENABLE_CPU>"
         "$<$<BOOL:${NOA_ENABLE_CUDA}>:NOA_ENABLE_CUDA>"
+        "$<$<BOOL:${NOA_ENABLE_UNIFIED}>:NOA_ENABLE_UNIFIED>"
         "$<$<BOOL:${NOA_ENABLE_TIFF}>:NOA_ENABLE_TIFF>"
         "$<$<BOOL:${NOA_ENABLE_OPENMP}>:NOA_ENABLE_OPENMP>"
         "$<$<BOOL:${NOA_FFTW_USE_THREADS}>:NOA_FFTW_USE_THREADS>"
