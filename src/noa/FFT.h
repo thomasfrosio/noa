ac859b574b0f0294270ba7a5d69d5dbcba5356b8
thomas <frosiot@gmail.com>
2022-09-11 15:46:10 +0100

    Add missing header...
    Tags: [fix]
    Compile: yes
    Tests: yes

--- src/noa/FFT.h
+++ src/noa/FFT.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #ifdef NOA_ENABLE_UNIFIED
+#include "noa/unified/fft/Factory.h"
 #include "noa/unified/fft/Remap.h"
 #include "noa/unified/fft/Resize.h"
 #include "noa/unified/fft/Transform.h"
