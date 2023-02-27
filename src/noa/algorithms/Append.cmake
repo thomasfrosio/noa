7ce764f37a839487e626fa4ddcb255846f13f99c
thomas <frosiot@gmail.com>
2023-02-27 02:59:12 +0000

    Update project files

--- src/noa/algorithms/Append.cmake
+++ src/noa/algorithms/Append.cmake
@@ -1,8 +1,10 @@
 # Included files for noa/algorithms:
 
 set(NOA_ALGO_HEADERS
+        algorithms/Utilities.hpp
+
         algorithms/memory/Arange.hpp
-        algorithms/memory/ExtractInsert.hpp
+        algorithms/memory/Subregion.hpp
         algorithms/memory/Iota.hpp
         algorithms/memory/Linspace.hpp
         algorithms/memory/Resize.hpp
@@ -15,13 +17,14 @@ set(NOA_ALGO_HEADERS
         algorithms/geometry/PolarTransform.hpp
         algorithms/geometry/PolarTransformRFFT.hpp
         algorithms/geometry/ProjectionsFFT.hpp
+        algorithms/geometry/Shape.hpp
         algorithms/geometry/Transform.hpp
         algorithms/geometry/TransformRFFT.hpp
-        algorithms/geometry/Utilities.hpp
 
-#        algorithms/signal/Shape.hpp
-#        algorithms/signal/FourierCorrelationPeak.hpp
-#        algorithms/signal/FSC.hpp
+        algorithms/signal/CorrelationPeak.hpp
+        algorithms/signal/FSC.hpp
+        algorithms/signal/Bandpass.hpp
+        algorithms/signal/PhaseShift.hpp
 
         )
 
