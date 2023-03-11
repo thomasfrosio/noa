c288d39e65eb70f5b329dbd6eafc0d5b2e3fdb22
thomas <frosiot@gmail.com>
2023-03-11 00:49:17 +0000

    Fix SFINAE

--- src/noa/gpu/cuda/memory/Arange.hpp
+++ src/noa/gpu/cuda/memory/Arange.hpp
@@ -5,10 +5,10 @@
 #include "noa/gpu/cuda/Stream.hpp"
 
 namespace noa::cuda::memory {
-    template<typename T, typename = std::enable_if_t<noa::traits::is_restricted_scalar_v<T>>>
+    template<typename T, typename = std::enable_if_t<noa::traits::is_restricted_numeric_v<T>>>
     void arange(T* src, i64 elements, T start, T step, Stream& stream);
 
-    template<typename T, typename = std::enable_if_t<noa::traits::is_restricted_scalar_v<T>>>
+    template<typename T, typename = std::enable_if_t<noa::traits::is_restricted_numeric_v<T>>>
     void arange(T* src, const Strides4<i64>& strides, const Shape4<i64>& shape,
                 T start, T step, Stream& stream);
 }
