8f8b764bddfd0d93d78a7bef01436822d134878e
thomas <frosiot@gmail.com>
2023-06-23 23:42:15 +0100

    Add simple tests for `reduce_unary()` and `reduce_binary()`

--- tests/noa/unified/TestUnifiedReduce.cpp
+++ tests/noa/unified/TestUnifiedReduce.cpp
@@ -8,6 +8,7 @@
 #include <catch2/catch.hpp>
 
 #include "Helpers.h"
+#include "noa/core/types/Functors.hpp"
 
 using namespace ::noa;
 
@@ -19,22 +20,19 @@ TEST_CASE("unified::reduce_unary", "[noa][unified]") {
     const auto shape = test::get_random_shape4_batched(3);
     const auto input = noa::math::random<f32>(noa::math::uniform_t{}, shape, 0, 500);
 
-    const auto result = noa::reduce_unary(input, f32{0},
-                                          [](f32 v) { return static_cast<f32>(v); },
-                                          noa::plus_t{},
-                                          [](f32 v) { return static_cast<f32>(v); });
-    const auto expected = noa::math::sum(input);
+    const auto result = noa::reduce_unary(input, f32{0}, {}, noa::max_t{}, {});
+    const auto expected = noa::math::max(input);
     REQUIRE(result == expected);
 }
 
 TEST_CASE("unified::reduce_binary", "[noa][unified]") {
     const auto shape = test::get_random_shape4_batched(3);
-    const auto input = noa::math::random<f32>(noa::math::uniform_t{}, shape, 0, 500);
+    const auto input = noa::math::random<f32>(noa::math::uniform_t{}, shape, -5, 5);
 
-    const auto result = noa::reduce_binary(input, input, f32{0},
-                                           [](f32 lhs, f32 rhs) { return static_cast<f32>(lhs * rhs); },
+    const auto result = noa::reduce_binary(input, input, f64{0},
+                                           [](f32 lhs, f32 rhs) { return static_cast<f64>(lhs * rhs); },
                                            noa::plus_t{},
-                                           {});
-    const auto expected = noa::math::sum(input);
+                                           [](f64 lhs) { return static_cast<f32>(lhs); });
+    const auto expected = noa::math::sum(input, noa::square_t{});
     REQUIRE(result == expected);
 }
