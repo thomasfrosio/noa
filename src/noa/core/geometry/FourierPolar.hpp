de8c855dfdb7bb6d285b83fcbacb505749bf4414
thomas <frosiot@gmail.com>
2024-03-23 18:52:46 +0000

    Update `RotationalAverage` core operator to new format

--- src/noa/core/geometry/FourierPolar.hpp
+++ src/noa/core/geometry/FourierPolar.hpp
@@ -3,13 +3,13 @@
 #include "noa/core/geometry/Polar.hpp"
 
 namespace noa::geometry {
-    /// 3d iwise operator to compute the cartesian->polar transformation of 2d non-redundant centered FFT(s).
+    /// 3d iwise operator to compute the spectrum->polar transformation of 2d centered rFFT(s).
     template<typename Index, typename Coord, typename Interpolator, typename OutputAccessor>
     requires (nt::is_interpolator_nd<Interpolator, 2>::value and
               nt::is_accessor_pure_nd<OutputAccessor, 3>::value and
               nt::is_sint<Index>::value and
               nt::is_any<Coord, f32, f64>::value)
-    class FourierCartesian2Polar {
+    class Spectrum2Polar {
     public:
         using index_type = Index;
         using interpolator_type = Interpolator;
@@ -24,22 +24,22 @@ namespace noa::geometry {
                       (nt::is_complex_v<input_value_type> and nt::is_real_v<output_value_type>));
 
     public:
-        FourierCartesian2Polar(
-                const interpolator_type& cartesian,
-                const Shape4<index_type>& cartesian_shape,
+        Spectrum2Polar(
+                const interpolator_type& spectrum,
+                const Shape4<index_type>& spectrum_shape,
                 const output_accessor_type& polar,
                 const Shape4<index_type>& polar_shape,
                 coord2_type frequency_range,
                 bool frequency_range_endpoint,
                 const coord2_type& angle_range,
                 bool angle_range_endpoint
-        ) : m_cartesian(cartesian),
+        ) : m_spectrum(spectrum),
             m_polar(polar),
             m_start_angle(angle_range[0]),
             m_start_frequency(frequency_range[0])
         {
             NOA_ASSERT(polar_shape[2] > 1 && polar_shape[3] > 1);
-            NOA_ASSERT(polar_shape[1] == 1 && cartesian_shape[1] == 1);
+            NOA_ASSERT(polar_shape[1] == 1 && spectrum_shape[1] == 1);
             NOA_ASSERT(frequency_range[1] - frequency_range[0] >= 0);
 
             using linspace_t = Linspace<coord_type, index_type>;
@@ -47,8 +47,8 @@ namespace noa::geometry {
             m_step_frequency = linspace_t::from_range(frequency_range[0], frequency_range[1], polar_shape[3], frequency_range_endpoint).step;
 
             // Scale the frequency range to the polar dimension [0,width).
-            m_scale = coord2_type::from_vec(cartesian_shape.filter(2, 3).vec);
-            m_height_dc_center = static_cast<coord_type>(cartesian_shape[2] / 2);
+            m_scale = coord2_type::from_vec(spectrum_shape.filter(2, 3).vec);
+            m_height_dc_center = static_cast<coord_type>(spectrum_shape[2] / 2);
         }
 
         NOA_HD constexpr void operator()(index_type batch, index_type y, index_type x) const {
@@ -56,18 +56,18 @@ namespace noa::geometry {
             const coord_type phi = polar_coordinate[0] * m_step_angle + m_start_angle;
             const coord_type rho = polar_coordinate[1] * m_step_frequency + m_start_frequency;
 
-            coord2_type cartesian_coordinates = (rho * sincos(phi)) * m_scale;
+            coord2_type spectrum_coordinates = (rho * sincos(phi)) * m_scale;
             input_real_type conj = 1;
-            if (cartesian_coordinates[1] < 0) {
-                cartesian_coordinates = -cartesian_coordinates;
+            if (spectrum_coordinates[1] < 0) {
+                spectrum_coordinates = -spectrum_coordinates;
                 if constexpr (nt::is_complex_v<input_value_type>)
                     conj = -1;
             } else {
                 (void) conj;
             }
-            cartesian_coordinates[0] += m_height_dc_center;
+            spectrum_coordinates[0] += m_height_dc_center;
 
-            auto value = m_cartesian(cartesian_coordinates, batch);
+            auto value = m_spectrum(spectrum_coordinates, batch);
             if constexpr (nt::are_complex_v<input_value_type, output_value_type>)
                 value.imag *= conj;
 
@@ -78,7 +78,7 @@ namespace noa::geometry {
         }
 
     private:
-        interpolator_type m_cartesian;
+        interpolator_type m_spectrum;
         output_accessor_type m_polar;
         coord2_type m_scale;
         coord_type m_height_dc_center;
--- src/noa/core/geometry/RotationalAverage.hpp
+++ src/noa/core/geometry/RotationalAverage.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "noa/core/fft/Frequency.hpp"
+#include "noa/core/fft/RemapInterface.hpp"
 #include "noa/core/signal/CTF.hpp"
 #include "noa/core/utils/Atomic.hpp"
 #include "noa/core/Types.hpp"
@@ -14,43 +15,42 @@ namespace noa::geometry {
     /// * The frequencies are normalized, so input dimensions don't have to be equal.
     /// * The user sets the number of output shells, as well as the output frequency range.
     /// * If input is complex and output real, the input is preprocessed to abs(input)^2.
-    template<noa::fft::Remap REMAP, size_t N,
-             typename Coord, typename Index, typename Offset,
-             typename Input, typename Output, typename CTF = Empty>
+    template<noa::fft::RemapInterface Remap,
+             size_t N, typename Coord, typename Index,
+             typename InputAccessor,    // Accessor<f32|f64|c32|c64, N+1>
+             typename OutputAccessor,   // Accessor<f32|f64|c32|c64, 2>
+             typename WeightAccessor,   // Accessor<f32|f64, 2>
+             typename CTFAccessor>      // Accessor<CTF, 1>, AccessorValue<CTF> or Empty
+    requires (nt::is_sint_v<Index> and nt::is_real_v<Coord> and
+              nt::is_accessor_nd_v<InputAccessor, N + 1> and
+              nt::are_accessor_nd_v<2, OutputAccessor, WeightAccessor> and
+              ((N == 2 and nt::is_accessor_nd_v<CTFAccessor, 1>) or std::is_empty_v<CTFAccessor>))
     class RotationalAverage {
     public:
-        static_assert(REMAP == noa::fft::H2H || REMAP == noa::fft::HC2H ||
-                      REMAP == noa::fft::F2H || REMAP == noa::fft::FC2H);
-        static_assert(nt::is_sint_v<Index>);
-        static_assert(nt::is_int_v<Offset>);
-        static_assert(nt::is_real_v<Coord>);
-        static_assert((nt::are_all_same_v<Input, Output> and
-                       nt::are_real_or_complex_v<Input, Output>) ||
-                      (nt::is_complex_v<Input> and
-                       nt::is_real_v<Output>));
-        static_assert((N == 2 and
-                       (nt::is_ctf_anisotropic_v<CTF> ||
-                        (std::is_pointer_v<CTF> and nt::is_ctf_anisotropic_v<nt::remove_pointer_cv_t<CTF>>))) ||
-                      std::is_empty_v<CTF>);
-
-        static constexpr bool IS_CENTERED = static_cast<u8>(REMAP) & noa::fft::Layout::SRC_CENTERED;
-        static constexpr bool IS_HALF = static_cast<u8>(REMAP) & noa::fft::Layout::SRC_HALF;
+        static_assert(Remap.is_xx2h());
+        static constexpr bool is_centered = Remap.is_xc2xx();
+        static constexpr bool is_rfft = Remap.is_hc2xx();
 
         using index_type = Index;
-        using offset_type = Offset;
         using coord_type = Coord;
-        using input_type = Input;
-        using output_type = Output;
-        using real_type = nt::value_type_t<output_type>;
-        using ctf_type = CTF;
-
-        using shape_type = Shape<index_type, N - IS_HALF>;
+        using shape_type = Shape<index_type, N - is_rfft>;
         using coord_nd_type = Vec<coord_type, N>;
         using coord2_type = Vec2<coord_type>;
         using shape_nd_type = Shape<index_type, N>;
-        using input_accessor_type = AccessorRestrict<const input_type, (N + 1), offset_type>;
-        using output_accessor_type = AccessorRestrictContiguous<output_type, 2, offset_type>;
-        using weight_accessor_type = AccessorRestrictContiguous<real_type, 2, offset_type>;
+
+        using input_accessor_type = InputAccessor;
+        using output_accessor_type = OutputAccessor;
+        using weight_accessor_type = WeightAccessor;
+        using input_type = nt::mutable_value_type_t<input_accessor_type>;
+        using output_type = nt::value_type_t<output_accessor_type>;
+        using weight_type = nt::value_type_t<weight_accessor_type>;
+        static_assert(nt::are_complex_v<input_type, output_type> or
+                      nt::are_real_v<input_type, output_type> or
+                      (nt::is_complex_v<input_type> and nt::is_real_v<output_type>));
+        static_assert(nt::is_real_v<weight_type>);
+
+        using ctf_type = nt::mutable_value_type_t<CTFAccessor>;
+        static_assert(nt::is_ctf_anisotropic_v<ctf_type>);
 
     public:
         RotationalAverage(
@@ -79,7 +79,7 @@ namespace noa::geometry {
             m_frequency_range_start = frequency_range[0];
             m_frequency_range_span = frequency_range[1] - frequency_range[0];
 
-            if constexpr (IS_HALF)
+            if constexpr (is_rfft)
                 m_shape = input_shape.pop_back();
             else
                 m_shape = input_shape;
@@ -88,8 +88,8 @@ namespace noa::geometry {
         // Batched 2d.
         NOA_HD void operator()(index_type batch, index_type iy, index_type ix) const noexcept requires (N == 2) {
             const auto frequency = coord_nd_type::from_values(
-                    noa::fft::index2frequency<IS_CENTERED>(iy, m_shape[0]),
-                    IS_HALF ? ix : noa::fft::index2frequency<IS_CENTERED>(ix, m_shape[1]));
+                    noa::fft::index2frequency<is_centered>(iy, m_shape[0]),
+                    is_rfft ? ix : noa::fft::index2frequency<is_centered>(ix, m_shape[1]));
             const auto fftfreq_2d = frequency * m_fftfreq_step;
 
             coord_type fftfreq;
@@ -97,10 +97,7 @@ namespace noa::geometry {
                 fftfreq = dot(fftfreq_2d, fftfreq_2d);
             } else {
                 // Correct for anisotropic pixel size and defocus.
-                if constexpr (std::is_pointer_v<ctf_type>)
-                    fftfreq = static_cast<coord_type>(m_ctf[batch].isotropic_fftfreq(fftfreq_2d));
-                else
-                    fftfreq = static_cast<coord_type>(m_ctf.isotropic_fftfreq(fftfreq_2d));
+                fftfreq = static_cast<coord_type>(m_ctf[batch].isotropic_fftfreq(fftfreq_2d));
             }
 
             if (fftfreq < m_frequency_range_sqd[0] or
@@ -117,9 +114,9 @@ namespace noa::geometry {
         // Batched 3d.
         NOA_HD void operator()(index_type batch, index_type iz, index_type iy, index_type ix) const noexcept requires (N == 3) {
             auto frequency = coord_nd_type::from_values(
-                    noa::fft::index2frequency<IS_CENTERED>(iz, m_shape[0]),
-                    noa::fft::index2frequency<IS_CENTERED>(iy, m_shape[1]),
-                    IS_HALF ? ix : noa::fft::index2frequency<IS_CENTERED>(ix, m_shape[2]));
+                    noa::fft::index2frequency<is_centered>(iz, m_shape[0]),
+                    noa::fft::index2frequency<is_centered>(iy, m_shape[1]),
+                    is_rfft ? ix : noa::fft::index2frequency<is_centered>(ix, m_shape[2]));
             frequency *= m_fftfreq_step;
 
             const auto fftfreq_sqd = dot(frequency, frequency);
@@ -133,36 +130,36 @@ namespace noa::geometry {
 
     private:
         NOA_HD static output_type input_to_output_(input_type input) noexcept {
-            if constexpr (nt::is_complex_v<input_type> and
-                          nt::is_real_v<output_type>) {
-                return abs_squared(input);
+            if constexpr (nt::is_complex_v<input_type> and nt::is_real_v<output_type>) {
+                return static_cast<output_type>(abs_squared(input));
             } else {
-                return input;
+                return static_cast<output_type>(input);
             }
         }
 
         NOA_HD void add_to_output_(index_type batch, coord_type fftfreq, output_type value) const noexcept {
             // Scale the normalized frequency back to the corresponding output shell.
-            const auto scaled_fftfreq = (fftfreq - m_frequency_range_start) / m_frequency_range_span;
-            const auto radius = scaled_fftfreq * static_cast<coord_type>(m_max_shell_index);
-            const auto radius_floor = floor(radius);
+            const coord_type scaled_fftfreq = (fftfreq - m_frequency_range_start) / m_frequency_range_span;
+            const coord_type radius = scaled_fftfreq * static_cast<coord_type>(m_max_shell_index);
+            const coord_type radius_floor = floor(radius);
 
             // Since by this point fftfreq has to be within the output frequency range,
             // "radius" is guaranteed to be within [0, m_max_shell_index].
             NOA_ASSERT(radius >= 0 and radius <= static_cast<coord_type>(m_max_shell_index));
 
             // Compute lerp weights.
-            const auto shell_low = static_cast<index_type>(radius_floor);
-            const auto shell_high = min(m_max_shell_index, shell_low + 1); // shell_low can be the last index
-            const auto fraction_high = static_cast<real_type>(radius - radius_floor);
-            const auto fraction_low = 1 - fraction_high;
+            const index_type shell_low = static_cast<index_type>(radius_floor);
+            const index_type shell_high = min(m_max_shell_index, shell_low + 1); // shell_low can be the last index
+            const coord_type fraction_high = radius - radius_floor;
+            const coord_type fraction_low = 1 - fraction_high;
 
-            // TODO In CUDA, we could do the atomic reduction in shared memory to reduce global memory transfers.
-            ng::atomic_add(m_output, value * fraction_low, batch, shell_low);
-            ng::atomic_add(m_output, value * fraction_high, batch, shell_high);
+            // TODO In CUDA, we could do the atomic reduction in shared memory to reduce global memory transfers?
+            using output_real_t = nt::value_type_t<output_type>;
+            ng::atomic_add(m_output, value * static_cast<output_real_t>(fraction_low), batch, shell_low);
+            ng::atomic_add(m_output, value * static_cast<output_real_t>(fraction_high), batch, shell_high);
             if (m_weight) {
-                ng::atomic_add(m_weight, fraction_low, batch, shell_low);
-                ng::atomic_add(m_weight, fraction_high, batch, shell_high);
+                ng::atomic_add(m_weight, static_cast<weight_type>(fraction_low), batch, shell_low);
+                ng::atomic_add(m_weight, static_cast<weight_type>(fraction_high), batch, shell_high);
             }
         }
 
