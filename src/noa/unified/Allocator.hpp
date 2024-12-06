a1f8db736abdd0f2c3783bad7ced9ce9366017e6
Thomas Frosio <frosiot@gmail.com>
2024-12-06 08:37:10 +0100

    Update code doc

--- src/noa/unified/Allocator.hpp
+++ src/noa/unified/Allocator.hpp
@@ -23,34 +23,32 @@ namespace noa::inline types {
             NONE = 0,
 
             /// The device default allocator.
-            /// - \b Allocation: For CPUs, it refers to the standard allocator using the heap as resource and
-            ///   returning at least 64-bytes aligned pointer. For GPUs, it refers to the GPU backend default
-            ///   allocator using the GPU global memory as resource. In CUDA, pointers have a minimum 256-bytes
-            ///   alignment. Allocations do not use the current stream.
-            /// - \b Accessibility: Allocated memory is private to the device that performed the allocation,
+            /// - \b Allocation: For CPUs, it refers to the standard allocator using the heap as memory resource and
+            ///   returning at least 64-bytes aligned pointer. For GPUs, it refers to the GPU backend's default
+            ///   allocator using the GPU global memory as a resource. In CUDA, pointers have a minimum 256-bytes
+            ///   alignment.
+            /// - \b Accessibility: The allocated memory is private to the device that performed the allocation,
             ///   but can be used by any stream of that device.
             DEFAULT = 1,
 
             /// The device asynchronous allocator.
             /// - \b Allocation: Same as DEFAULT, except if the device is a CUDA-capable device. In this case,
-            ///   the current stream of the device is used to performed the allocation, which is thereby stream-
-            ///   ordered. Since CUDA 11.2, it is the recommend way to allocate GPU memory. The alignment is
-            ///   the same as DEFAULT.
+            ///   the current stream of the device is used to perform the allocation, i.e. the allocation is
+            ///   stream-ordered. Since CUDA 11.2, it's been the recommended way to allocate GPU memory.
             /// - \b Accessibility: Allocated memory is private to the device that performed the allocation,
             ///   but can be used by any stream of that device. If the device is a CUDA-capable device, one
-            ///   should make sure the memory is accessed in the appropriate stream-order after allocation since
-            ///   the memory is only valid when the stream reaches the allocation event.
+            ///   should make sure the memory is accessed in a stream-ordered way since the memory is only
+            ///   valid when the stream reaches the allocation event.
             DEFAULT_ASYNC = 2,
             ASYNC = DEFAULT_ASYNC,
 
             /// "Pitch" allocator.
             /// - \b Allocation: This is equivalent to DEFAULT, except for CUDA-capable devices. In this case,
-            ///   the CUDA driver will potentially pad the right side of the innermost dimension of the ND array.
-            ///   The size of the innermost dimension, including the padding, is called the "pitch". "Pitched"
-            ///   layouts can be useful to minimize the number of memory accesses on a given row (but can increase
-            ///   the number of memory accesses for reading the whole array) and to reduce shared memory bank
-            ///   conflicts. It is highly recommended to use these layouts if the application will be performing
-            ///   memory copies involving 2D or 3D CUDA arrays. Allocations do not use the current stream.
+            ///   the CUDA driver will potentially pad the right side of each row of the ND array. The size of the row,
+            ///   including the padding, is called the "pitch". "Pitched" layouts can be useful to minimize the number
+            ///   of memory accesses on a given row (but may can increase the number of memory accesses for reading
+            ///   the whole array) and to reduce shared memory bank conflicts. It is recommended to use these layouts
+            ///   if the application is performing copies from/to 2d or 3d CUDA arrays.
             /// - \b Accessibility: Allocated memory is private to the device that performed the allocation,
             ///   but can be used by any stream of that device.
             PITCHED = 4,
@@ -59,10 +57,11 @@ namespace noa::inline types {
             /// - \b Allocation: Pinned memory can be allocated by a CPU or a GPU device. Allocating excessive
             ///   amounts of pinned memory may degrade system performance, since it reduces the amount of memory
             ///   available to the system for paging. Thus, it is best used sparingly, e.g. to allocate staging
-            ///   areas for data exchange between CPU and GPU. Allocations do not use the current stream.
+            ///   areas for data exchange between CPU and GPU. Note that accessing pinned memory from the GPU in
+            ///   a non-coalesced way may result in terribly poor performance.
             /// - \b Accessibility: Can be accessed by the CPU, and the GPU against which the allocation was
             ///   performed. If the CPU device was used for allocation, this GPU is the "current" GPU at the
-            ///   time of allocation.
+            ///   time of allocation. Concurrent access from the CPU and the GPU is illegal.
             PINNED = 8,
 
             /// Managed memory allocator.
@@ -71,15 +70,16 @@ namespace noa::inline types {
             ///   streams are used (the memory is attached to them), the allocation itself is synchronous.
             /// - \b Accessibility: Can be accessed by the CPU. If the GPU stream used for the allocation
             ///   was the NULL stream, this is equivalent to MANAGED_GLOBAL. Otherwise, the allocated memory on
-            ///   the GPU side is private to the stream and the GPU that performed the allocation.
+            ///   the GPU side is private to the stream and the GPU that performed the allocation. Concurrent access
+            ///   from the CPU and the GPU is illegal.
             MANAGED = 16,
             UNIFIED = MANAGED,
 
             /// Managed memory allocator.
-            /// - \b Allocation: Managed memory can be allocated by a CPU or a GPU device. Allocation does not
-            ///   use the current stream. Note that this is much less efficient compared to a stream-private
-            ///   allocation with MANAGED.
-            /// - \b Accessibility: Can be accessed by any stream and any device (CPU and GPU).
+            /// - \b Allocation: Managed memory can be allocated by a CPU or a GPU device. In CUDA, this is much less
+            ///   efficient compared to a stream-private allocation with MANAGED.
+            /// - \b Accessibility: Can be accessed by any stream and any device (CPU and GPU). Concurrent access
+            ///   from the CPU and the GPU is illegal.
             MANAGED_GLOBAL = 32,
             UNIFIED_GLOBAL = MANAGED_GLOBAL,
 
--- src/noa/unified/ArrayOption.hpp
+++ src/noa/unified/ArrayOption.hpp
@@ -21,11 +21,12 @@ namespace noa::inline types {
         }
 
         /// Whether the allocated data can be accessed by CPU threads.
-        /// \note While it indicates whether the managed data can be read/written on the CPU, it does not indicate
-        ///       how the managed data is used. This choice is purely made on the device currently associated with
-        ///       the allocated data. For instance, pinned memory can be dereferenced by the CPU, so this function will
-        ///       returned true, but if the device is a GPU, the library will refer to the memory region as a
-        ///       GPU-own region and will therefore prioritize GPU access.
+        /// \note While it indicates whether the managed data can be read/written by the CPU, it does not indicate
+        ///       how the library uses the managed data. Indeed, this choice is purely made on the device currently
+        ///       associated with the allocated data. For instance, the CPU may access values from a pinned GPU array
+        ///       (certain conditions may apply, see Allocator), i.e. it is dereferenceable. However, since the device
+        ///       is a GPU, the library will refer to the memory region as GPU-owned and will therefore prioritize GPU
+        ///       access.
         [[nodiscard]] constexpr auto is_dereferenceable() const noexcept -> bool {
             return device.is_cpu() or allocator.is_any(Allocator::PINNED, Allocator::MANAGED, Allocator::MANAGED_GLOBAL);
         }
