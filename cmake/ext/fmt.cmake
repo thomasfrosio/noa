3057765a7a015ab89da69e81fdc6b536c2909077
Thomas Frosio <frosiot@gmail.com>
2025-09-08 17:27:16 +0100

    upgrade `{fmt}` for LLVM 21.1 on MacOS.

--- cmake/ext/fmt.cmake
+++ cmake/ext/fmt.cmake
@@ -5,7 +5,7 @@ if (TARGET fmt::fmt)
     message(STATUS "Target already exists: fmt::fmt")
 else ()
     set(fmt_REPOSITORY https://github.com/fmtlib/fmt)
-    set(fmt_TAG 11.1.4)
+    set(fmt_TAG 53d006abfdc0653f7d3e4e180e694fcb720524b5) # Sep 7, 2025, v11.2.1, up from v11.1.4 to fix #4477
 
     message(STATUS "Repository: ${fmt_REPOSITORY}")
     message(STATUS "Git tag: ${fmt_TAG}")
