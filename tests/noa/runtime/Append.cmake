cd7c877ef987aca3955a5a2b360bdb6a4743bc97
thomas <frosiot@gmail.com>
2026-07-16 18:10:59 +0200

    Enable tests

--- tests/noa/runtime/Append.cmake
+++ tests/noa/runtime/Append.cmake
@@ -16,27 +16,27 @@ list(APPEND TEST_SOURCES
     noa/runtime/cpu/TestRuntimeCPUReduceIwise.cpp
     noa/runtime/cpu/TestRuntimeCPUStream.cpp
 
-#    noa/runtime/TestRuntimeArray.cpp
-#    noa/runtime/TestRuntimeBlas.cpp
-#    noa/runtime/TestRuntimeCast.cpp
-#    noa/runtime/TestRuntimeComplex.cpp
-#    noa/runtime/TestRuntimeCopy.cpp
-#    noa/runtime/TestRuntimeDevice.cpp
-#    noa/runtime/TestRuntimeEwise.cpp
-#    noa/runtime/TestRuntimeFactory.cpp
+    noa/runtime/TestRuntimeArray.cpp
+    noa/runtime/TestRuntimeBlas.cpp
+    noa/runtime/TestRuntimeCast.cpp
+    noa/runtime/TestRuntimeComplex.cpp
+    noa/runtime/TestRuntimeCopy.cpp
+    noa/runtime/TestRuntimeDevice.cpp
+    noa/runtime/TestRuntimeEwise.cpp
+    noa/runtime/TestRuntimeFactory.cpp
 #    noa/runtime/TestRuntimeHistogram.cpp
-#    noa/runtime/TestRuntimeIwise.cpp
-#    noa/runtime/TestRuntimePermute.cpp
-#    noa/runtime/TestRuntimeRandom.cpp
+    noa/runtime/TestRuntimeIwise.cpp
+    noa/runtime/TestRuntimePermute.cpp
+    noa/runtime/TestRuntimeRandom.cpp
 #    noa/runtime/TestRuntimeReduce.cpp
 #    noa/runtime/TestRuntimeReduceAxes.cpp
 #    noa/runtime/TestRuntimeReduceBatch.cpp
 #    noa/runtime/TestRuntimeReduceEwise.cpp
 #    noa/runtime/TestRuntimeReduceIwise.cpp
-#    noa/runtime/TestRuntimeResize.cpp
-#    noa/runtime/TestRuntimeSort.cpp
-#    noa/runtime/TestRuntimeStream.cpp
-#    noa/runtime/TestRuntimeSubregion.cpp
+    noa/runtime/TestRuntimeResize.cpp
+    noa/runtime/TestRuntimeSort.cpp
+    noa/runtime/TestRuntimeStream.cpp
+    noa/runtime/TestRuntimeSubregion.cpp
 )
 
 if (NOA_ENABLE_CUDA)
