8fc0e4df7a031639a7a9ff645afe44ba6c81ca31
thomas <frosiot@gmail.com>
2026-08-04 10:40:53 +0200

    remove `BatchedParameter` and related traits

--- src/noa/runtime/core/Traits.hpp
+++ src/noa/runtime/core/Traits.hpp
@@ -69,8 +69,6 @@ namespace noa::traits {
     template<typename... T> concept tuple_of_accessor_pure_or_empty = conjunction_or_false<is_tuple_of_accessor_pure_or_empty<T>...>::value;
     template<typename... T> concept tuple_of_accessor_value_or_empty = conjunction_or_false<is_tuple_of_accessor_value_or_empty<T>...>::value;
 
-    NOA_GENERATE_PROCLAIM_FULL(batched_parameter);
-
     NOA_GENERATE_PROCLAIM_FULL(span);
     NOA_GENERATE_PROCLAIM_FULL(span_contiguous);
     NOA_GENERATE_PROCLAIM_FULL_ND(span_nd);
--- src/noa/runtime/core/Utils.hpp
+++ src/noa/runtime/core/Utils.hpp
@@ -332,31 +332,4 @@ namespace noa::details {
 
     template<typename T, usize N, nt::integer I>
     using ReinterpretLayoutContiguous = ReinterpretLayout<T, N, I, StridesTraits::CONTIGUOUS>;
-
-    template<typename T>
-    struct BatchedParameter {
-        static constexpr bool IS_BATCHED = nt::pointer<T> or nt::accessor_nd<T, 1> or nt::span_nd<T, 1>;
-        using type = T;
-        using value_type = std::conditional_t<IS_BATCHED, nt::value_type_t<T>, T>;
-
-        constexpr auto& operator[](nt::integer auto i) const noexcept {
-            if constexpr (IS_BATCHED)
-                return value[i];
-            else
-                return value;
-        }
-
-        constexpr auto& operator[](nt::integer auto i) noexcept {
-            if constexpr (IS_BATCHED)
-                return value[i];
-            else
-                return value;
-        }
-
-        NOA_NO_UNIQUE_ADDRESS T value;
-    };
-}
-
-namespace noa::traits {
-    template<typename T> struct proclaim_is_batched_parameter<nd::BatchedParameter<T>> : std::true_type {};
 }
